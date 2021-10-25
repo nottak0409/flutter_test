cbed59f63f5756202399abf1d8e0f259051c6229
nottak0409 <nottak0409@gmail.com>
2021-10-25 17:17:04 +0900

    一覧表示作成

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -29,8 +29,29 @@ class TodoListPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('リスト一覧'),
       ),
-      body: Center(
-        child: Text('リスト一覧画面'),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('人参を買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('玉ねぎを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('じゃがいもを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('カレールーを買う'),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
